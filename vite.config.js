e64a0d4434187698a63eb2014254ce6cf3539ef7
fenglibo51 <127223534+fenglibo51@users.noreply.github.com>
2026-03-15 17:49:36 +0800

    feat: 修正目录结构更新图标

--- vite.config.js
+++ vite.config.js
@@ -29,9 +29,15 @@ export default defineConfig({
         start_url: '/',
         icons: [
           {
-            src: '/favicon.ico',     
-            sizes: '192x192 512x512',
-            type: 'image/x-icon'
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
           }
         ]
       }
