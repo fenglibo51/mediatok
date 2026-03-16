bd735e95a0e1d18c3e5375cfda0bf0702231a716
fenglibo51 <127223534+fenglibo51@users.noreply.github.com>
2026-03-16 14:57:33 +0800

    继续修热更新，也不知道好了没

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -399,7 +399,7 @@ onMounted(() => {
               <span class="text-zinc-950 font-black text-4xl tracking-tighter">MT</span>
             </div>
             <h1 class="text-3xl font-black text-white mb-1">MEDIA-TOK</h1>
-            <p class="text-emerald-400 font-black text-[11px] tracking-[0.15em] mb-6 uppercase leading-relaxed">VERSION 1.0.4<br>(ALPHA)</p>
+            <p class="text-emerald-400 font-black text-[11px] tracking-[0.15em] mb-6 uppercase leading-relaxed">VERSION 1.0.5<br>(ALPHA)</p>
 
             <div class="flex flex-col items-center gap-2 mb-8 w-full">
               <div class="flex flex-wrap justify-center gap-2">
